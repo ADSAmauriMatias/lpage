bc565df7cc326f10daefde5ad8bc04641a7f2eef
Amauri Matias <adsmatias@outlook.com>
2025-05-15 21:16:28 -0300

    Adição da tag google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,20 @@ export default function RootLayout({
   return (
     <html lang="en" className="scroll-smooth">
       <head>
+         {/* Google Tag Manager */}
+        <Script
+          id="gtm-head"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MHKLLZVF');
+            `,
+          }}
+        />
         {/* Google Analytics Script */}
         <Script
           strategy="afterInteractive"
@@ -49,10 +63,20 @@ export default function RootLayout({
             `,
           }}
         />
+
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MHKLLZVF"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <Header />
         {children}
         <AosInit/>
